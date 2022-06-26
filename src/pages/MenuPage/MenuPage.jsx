d21876b6b167596a45ecea0cb14ad5a5fd4a58b2
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-06-25 17:06:44 -0700

    styles menu page, added items to menu list

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -6,75 +6,287 @@ export default function MenuPage() {
         <>
             <main className="menu-container">
                 <h1 className="menu-title">MENU</h1>
+                <h2>DONT FORGET TO REMOVE BORDERS</h2>
                 <section className="menu-appetizers-container">
                     <div className="menu-category-containers">
                         <h1 className="menu-category-titles">APPETIZERS</h1>
                     </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">PRETZEL BITES</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CHICKEN TENDERS</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">ONION RINGS</h4>
+                            <p className="menu-body-descriptions">
+                                12 crispy onion rings with spicy ranch and 
+                                our special mayo
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CALAMARI</h4>
+                            <p className="menu-body-descriptions">
+                                Tender calamari, breaded and fried with our special batter.
+                                Served with marinara sauce and spicy ranch
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CHICKEN WINGS</h4>
+                            <p className="menu-body-descriptions">
+                                Tossed in spicy buffalo sauce and served with
+                                celery and ranch for dipping
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">MOZZARELLA</h4>
+                            <p className="menu-body-descriptions">
+                                Hand-breaded in herb-panko breadcrumbs,
+                                fried and served with marinara sauce
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">FRIED PICKLES</h4>
+                            <p className="menu-body-descriptions">
+                                25 Light breaded dill slices, fried and served with 
+                                your choice of sauce
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">FAMILY NACHOS</h4>
+                            <p className="menu-body-descriptions">
+                                Nachos topped with your choice of meat, cheddar cheese, olives, peppers, onion, and
+                                jalepenos. Served with guacamole and sour creme
+                            </p>
+                        </div>
+                    </div>
                 </section>
                 <section className="menu-salads-container">
-                <div className="menu-category-containers">
-                    <h1 className="menu-category-titles">SALADS</h1>
-                </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-category-containers">
+                        <h1 className="menu-category-titles">SALADS</h1>
+                    </div>
+                    <div className="menu-body-container" >
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CAESAR SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CHEF SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">BBQ CHICKEN SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">COBB SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">ZACH'S HOUSE SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CALI ZEST SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">SPECIAL PEAR SALAD</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        
+                    </div>
                 </section>
                 <section className="menu-burgers-container">
-                <div className="menu-category-containers">
-                    <h1 className="menu-category-titles">BURGERS</h1>
-                </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-category-containers">
+                        <h1 className="menu-category-titles">BURGERS</h1>
+                    </div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CLASSIC CHEESEBURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">BACON BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">BREAKFAST BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">HAWAIIN BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">BBQ BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">LA FRESCA BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">COLOSSAL BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">IMPOSSIBLE CHEESEBURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">TUNA MELT</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                    </div>
                 </section>
                 <section className="menu-kids-container">
                 <div className="menu-category-containers">
                     <h1 className="menu-category-titles">KIDS</h1>
                 </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">LIL BURGER</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">LIL MAC & CHEESE</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">KIDS CHICKEN TENDERS</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">LIL NUGS</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                    </div>
+                   
                 </section>
                 <section className="menu-desserts-container">
                 <div className="menu-category-containers">
                     <h1 className="menu-category-titles">DESSERTS</h1>
                 </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">BANANA SPLIT</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">ITALIAN DOUGHNUTS</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CINIMON BITES</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">TIRAMISU</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">SPECIAL CHEESECAKE</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">CHOCOLATE BROWNIE CAKE</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">MILKSHAKES</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                    </div>
                 </section>
                 <section className="menu-specials-container">
                 <div className="menu-category-containers">
                     <h1 className="menu-category-titles">SPECIALS</h1>
                 </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">PRETZEL BITES</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div>
+                    </div>
+                    
                 </section>
                 <section className="menu-drinks-container">
                 <div className="menu-category-containers">
                     <h1 className="menu-category-titles">DRINKS</h1>
                 </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div className="menu-body-container">
+                        <div className="menu-body">
+                            <h4 className="menu-body-titles">PRETZEL BITES</h4>
+                            <p className="menu-body-descriptions">
+                                Served with cheddar cheese sauce and mustard
+                            </p>
+                        </div> 
+                    </div>
+                   
                 </section>
             </main>
         </>
