1a4bdd2e121140f1b0d3d39e3da9190c4d7bf9e2
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2024-03-29 21:14:04 -0700

    testing deployment

--- config/ensureLoggedIn.js
+++ config/ensureLoggedIn.js
@@ -1,4 +1,4 @@
 module.exports = (req, res, next) => {
-  if (!req.user) return res.status(401).json("Unauthorized");
+  if (!req.user) return res.status(401).json("Unauthorized, You need to be logged in to use this feature");
   next();
 };
